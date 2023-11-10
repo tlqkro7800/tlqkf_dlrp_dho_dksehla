335e0642dd250a194937e9349fa973d0ff09d8a2
tlqkro7800 <126274002+tlqkro7800@users.noreply.github.com>
2023-11-10 20:35:37 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "fuck_final_final_final_final"
+include(":app")
+ 
